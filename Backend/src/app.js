5c166bf12a0394c4d3a30b1c67b9d0916a0b2433
Rgulati4657 <100552980+Rgulati4657@users.noreply.github.com>
2025-04-10 18:13:56 +0530

    updated code handling login and signup

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,18 +3,34 @@ const cors = require('cors');
 
 const authRoutes = require('./routes/authRoutes'); // Import Routes
 const generalRoutes = require('./routes/generalRoutes'); // Import Routes
+const chatRoutes = require('./routes/chatRoutes'); // Import Routes
+const domainRoutes = require('./routes/domainRoutes'); // Import Routes
+const errorHandler = require('./middleware/errorMiddleware');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 
 
 
 
 // Routes
-app.use('/api/auth', authRoutes);
+app.use('/api/auth', authRoutes); //cred routes
 app.use('/',generalRoutes) // general Routes
+app.use('/api/chat', chatRoutes); // chat routes
+app.use('/api/domain', domainRoutes); // websitedomain route validator
+
+
+
+// Handle unknown routes
+app.use((req, res, next) => {
+    res.status(404).json({ success: false, message: 'Route not found' });
+  });
+
+ // Global Error Handling Middleware
+app.use(errorHandler);
+
 
 module.exports = app;
 
--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,11 +6,28 @@ import { Route, Routes } from "react-router";
 // import SignUp1 from "./Components/signUp_Login/SignUp1";
 // import SignUp2 from "./Components/signUp_Login/signUp2";
 import SignUpForm from "./Components/signUp_Login/signUpForm";
-
+import LoginForm from "./Components/signUp_Login/LoginForm";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Inbox from "./Components/Dashboard/Inbox";
 
 function App() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/") // Change to your backend API endpoint
+      .then((response) => { setData(response.data) 
+
+        console.log(response)
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+
+    console.log("hello",data);
+  }, []);
   return (
     <div className="App">
+      {data}
       {/* <Navbar/> */}
       {/* <NavBar/> */}
       {/* <HomeFeature />
@@ -21,13 +38,16 @@ function App() {
       {/* <SignUp1/> */}
       {/* <SignUp2/> */}
 
-      <SignUpForm/>
-   
+     
+
       <Routes>
-        <Route>
+        <Route path="/signup" element={ <SignUpForm />} />
+        <Route path='/login' element={<LoginForm/>} />
+        <Route path="/inbox" element={<Inbox/>} />
+          
           {/* <Route path="/" element={<Navbar />} /> */}
           {/* <Route path="/apps" element= */}
-        </Route>
+        
       </Routes>
     </div>
   );
